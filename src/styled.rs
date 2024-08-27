bf9f6834427aa5932dd2ac06c734c9dab1c83acb
Sam McGowan <the.sam.mcg@protonmail.com>
2024-08-27 12:43:31 +0100

    Reintroduce simple `Styled` type.

--- src/styled.rs
+++ src/styled.rs
@@ -5,48 +5,61 @@ pub use std::fmt::Formatter;
 #[macro_export]
 macro_rules! styled {
     ($($tt:tt)+) => {
-        $crate::styled::Styled::new(|f, style| {
-                let reset_style = $crate::styled::RESET_STYLE.get();
-
-                $crate::styled::RESET_STYLE.set(style);
-
-                write!(f, "{style}")?;
-                write!(f, $($tt)+)?;
-                write!(f, "{reset_style}")?;
-
-
-                $crate::styled::RESET_STYLE.set(reset_style);
-
-                Ok(())
-        })
+        $crate::styled::Styled::new($crate::styled::FormatArgsCallback::new(|f| write!(f, $($tt)+)))
     };
 }
 
 use std::cell::Cell;
 use std::fmt;
 
-pub struct Styled<F> {
-    callback: F,
+pub struct Styled<T> {
     pub style: Style,
+    pub content: T,
 }
 
-impl<F: Fn(&mut fmt::Formatter, Style) -> fmt::Result> Styled<F> {
-    pub const fn new(callback: F) -> Self {
+impl<T> Styled<T> {
+    #[inline]
+    pub const fn new(content: T) -> Self {
         Self {
-            callback,
             style: Style::new(),
+            content,
         }
     }
 }
 
-impl<F: Fn(&mut fmt::Formatter, Style) -> fmt::Result> fmt::Display for Styled<F> {
-    #[inline]
+impl<F: fmt::Display> fmt::Display for Styled<F> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        (self.callback)(f, self.style)
+        let reset_style = RESET_STYLE.get();
+
+        RESET_STYLE.set(self.style);
+
+        self.style.fmt(f)?;
+        self.content.fmt(f)?;
+        reset_style.fmt(f)?;
+
+        RESET_STYLE.set(reset_style);
+
+        Ok(())
     }
 }
 
 thread_local! {
     #[doc(hidden)]
     pub static RESET_STYLE: Cell<Style> = const { Cell::new(Style::new()) };
+}
+
+pub struct FormatArgsCallback<F>(F);
+
+impl<F: Fn(&mut fmt::Formatter) -> fmt::Result> FormatArgsCallback<F> {
+    #[inline]
+    pub const fn new(callback: F) -> Self {
+        Self(callback)
+    }
+}
+
+impl<F: Fn(&mut fmt::Formatter) -> fmt::Result> fmt::Display for FormatArgsCallback<F> {
+    #[inline]
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        (self.0)(f)
+    }
 }
